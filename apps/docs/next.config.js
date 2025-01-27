efb1fcb76df549d78d3115cc1941f67b8c15c37e
Thai Hoang <mr.hoangminhthai@gmail.com>
2025-01-27 11:09:30 +0700

    chore: remove console logs from next.config.js and add turbo-cache.json for Remote Cache API

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,9 +3,6 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
-console.log('process.env.NODE_VERSION:', process.env.NODE_VERSION)
-
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
