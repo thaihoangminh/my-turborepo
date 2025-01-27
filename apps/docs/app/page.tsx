4c6d2de4d53076debb7b774502fcc110f97533a3
Thai Hoang <mr.hoangminhthai@gmail.com>
2025-01-27 08:32:53 +0700

    revert: page.tsx

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/docs/app/page.tsx {process.env.NEXT_PUBLIC_ANALYTICS_ID ?? 'undefined'}</code>
+            Get started by editing <code>apps/docs/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
