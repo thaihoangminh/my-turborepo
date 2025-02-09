1fe47532d862223f1184f097803f24c355ba48a2
Thai Hoang <mr.hoangminhthai@gmail.com>
2025-02-09 16:03:27 +0700

    feat: enhance Docker setup with web service and environment variable support

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</div>
         <ThemeImage
           className={styles.logo}
           srcLight="turborepo-dark.svg"
